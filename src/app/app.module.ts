e5966df69d13a1b5fa45ea864caeaf109f63f6d9
nzansv <63594033+nzansv@users.noreply.github.com>
2021-04-29 04:22:18 +0600

    *

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,32 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './const/header/header.component';
 import {FooterComponent} from './const/footer/footer.component';
 import {HttpClientModule} from '@angular/common/http';
+import {GteDirective} from './gte.directive';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { ModelDrivenFormComponent } from './const/footer/model-driven-form/model-driven-form.component';
+import {GteValidatorService} from './const/footer/model-driven-form/gte-validator.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    GteDirective,
+    ModelDrivenFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+  ],
+  providers: [GteValidatorService],
   bootstrap: [AppComponent]
 })
 
--- src/app/service/events.service.ts
+++ src/app/service/events.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+  getEvents(): Observable<any> {
+    return this.http.get('assets/events.json');
+  }
 }
