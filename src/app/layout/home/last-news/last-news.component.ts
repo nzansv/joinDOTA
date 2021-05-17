9155946910b32c1dd4d886d35393ca901bd2528b
nzansv <63594033+nzansv@users.noreply.github.com>
2021-05-17 18:58:01 +0600

    *

--- src/app/layout/home/last-news/last-news.component.ts
+++ src/app/layout/home/last-news/last-news.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NewsService} from '../../../service/news.service';
+import {RestApiService} from '../../../service/rest-api.service';
 
 @Component({
   selector: 'app-last-news',
@@ -10,10 +11,19 @@ import {NewsService} from '../../../service/news.service';
 export class LastNewsComponent implements OnInit {
   news: any = [];
 
-  constructor(private newsService: NewsService) { }
+  News: any = [];
+
+  constructor(private newsService: NewsService, public restApi: RestApiService) { }
 
   ngOnInit(): void {
     this.getNews();
+    this.loadNews();
+  }
+
+  loadNews(): any {
+    return this.restApi.getEmployees().subscribe((data: {}) => {
+      this.News = data;
+    });
   }
 
   getNews(): any{
